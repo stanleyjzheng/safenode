19de0afa20b232d5f9b1291255ac8d945e84775a
stanleyjzheng <58539993+stanleyjzheng@users.noreply.github.com>
2022-06-25 02:59:43 -0400

    decode eth_sendRawTransaction in proxy

--- rpc/testing.js
+++ rpc/testing.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 const request = require('request');
+const toBuffer = require('ethereumjs-util').toBuffer
+const { FeeMarketEIP1559Transaction } = require('@ethereumjs/tx')
 
 const app = express();
 
@@ -28,8 +30,22 @@ async function passthroughRPC(req, res) {
 
 app.post('/', async function (req, res) {
     console.log('call', req.body)
-    if req.body['method'] == 'eth_sendRawTransaction'{
-        console.log('uhhh do something here')
+    if (req.body['method'] == 'eth_sendRawTransaction') {
+        console.log(req.body)
+        const txData = toBuffer(req.body['params'][0])
+        const tx = FeeMarketEIP1559Transaction.fromRlpSerializedTx(txData)
+        var tenderlyBody = {
+            "network_id": tx.chainId.toString('hex'),
+            "from": tx.getSenderAddress().toString('hex'),
+            "to": tx.to.toString('hex'),
+            "input": tx.data.toString('hex'),
+            "gas": tx.gasLimit.toNumber(),
+            "gas_price": tx.maxFeePerGas.toNumber() + tx.maxPriorityFeePerGas.toNumber(),
+            "value": tx.value.toNumber(),
+        }
+
+        // do somethin here with tenderly
+        console.log(tenderlyBody)
     }
     else {
         passthroughRPC(req, res)
